9d54832b0bdf18020f84af8c4c6bc882ce63ac87
jahiker <rojasjahiker@gmail.com>
2022-07-28 23:54:11 -0400

    teleports

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="toggle">Modal</button>
+    <teleport to="body">
+      <div v-show="show" class="modal">
+        <h1>Titulo</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam quae
+          commodi accusantium excepturi sunt esse velit dolore repellat
+          perferendis aliquid neque quidem sed, voluptates veritatis harum.
+          Tenetur et fugit illo?
+        </p>
+        <button @click="toggle">Cerrar</button>
+      </div>
+    </teleport>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
